30d728773c3e37728a833bd5c4e11ddb350846e0
alex.polak <alex.polak@interactiv4.com>
2021-08-22 15:24:38 +0300

    initial dist

---
+++ precache-manifest.c12c6dbfcb0d1119a9458171f1ac4b11.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33e781d1625dec566e77",
+    "url": "/css/app.dfa332cb.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "b322c4ae3dce0970c40bb311fc1e8e36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56e9241a008e5a6043e4",
+    "url": "/js/about.aef476b2.js"
+  },
+  {
+    "revision": "33e781d1625dec566e77",
+    "url": "/js/app.ee69119b.js"
+  },
+  {
+    "revision": "b90fac318007cf61c7cc",
+    "url": "/js/chunk-vendors.45d58b09.js"
+  },
+  {
+    "revision": "807e985c3190fad24407970484d7d8d4",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
